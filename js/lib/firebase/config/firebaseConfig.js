54261feff0c1cd6019ac96ecf34cbf97177a053f
Jack Hewertson <122547280+JackHewert@users.noreply.github.com>
2024-03-20 17:38:11 -0400

    lab 3 attempt 2

--- js/lib/firebase/config/firebaseConfig.js
+++ js/lib/firebase/config/firebaseConfig.js
@@ -1,19 +1,12 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+// firebaseConfig.js
 const firebaseConfig = {
-	apiKey: import.meta.env.VITE_API_KEY,
-	authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-	databaseURL: import.meta.env.VITE_DB_URL,
-	projectId: import.meta.env.VITE_PROJECT_ID,
-	storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-	messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-	appId: import.meta.env.VITE_APP_ID
+	apiKey: process.env.VITE_API_KEY,
+	authDomain: process.env.VITE_AUTH_DOMAIN,
+	databaseURL: process.env.VITE_DB_URL,
+	projectId: process.env.VITE_PROJECT_ID,
+	storageBucket: process.env.VITE_STORAGE_BUCKET,
+	messagingSenderId: process.env.VITE_MESSAGING_SENDER_ID,
+	appId: process.env.VITE_APP_ID
 };
 
-// Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-
-// Get a reference to the database service
-const database = getDatabase(firebaseApp);
-
-export { database };
+export { firebaseConfig };
